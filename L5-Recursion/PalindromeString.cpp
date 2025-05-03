0c246315f4edbadbc44cd7dccf0775cea59ec464
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-03 15:52:52 +0530

    palindome leetcode

---
+++ L5-Recursion/PalindromeString.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// leetcode
+bool isPalindrome(string &s)
+{
+    int left = 0;
+    int right = s.size() - 1;
+
+    while (left < right)
+    {
+        while (left < right && !isalnum(s[left]))
+        {
+            left++;
+        }
+        while (left < right && !isalnum(s[right]))
+        {
+            right--;
+        }
+
+        if (tolower(s[left]) != tolower(s[right]))
+        {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main()
+{
+    string s;
+    cout << "Enter a string : ";
+    getline(cin, s);
+    int l = s.size();
+    if (isPalindrome(s))
+    {
+        cout << "Yes";
+    }
+    else
+    {
+        cout << "No";
+    }
+}
