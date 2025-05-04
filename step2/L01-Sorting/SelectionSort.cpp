271b1626c4ae8d01545e85c0c667818c8fb99312
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-04 19:12:58 +0530

    Strivers method

--- step2/L01-Sorting/SelectionSort.cpp
+++ step2/L01-Sorting/SelectionSort.cpp
@@ -1,24 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+void selectionSort(int arr[], int size)
 {
-    int arr[] = {13, 46, 24, 52, 20, 9};
-    int size = sizeof(arr) / sizeof(arr[0]);
-
     for (int i = 0; i < size - 1; i++)
     {
-        int min = i;
+        int mini = i;
         for (int j = i; j < size; j++)
         {
-            if (arr[j] < arr[min])
+            if (arr[j] < arr[mini])
             {
-                min = j;
+                mini = j;
             }
         }
-        swap(arr[i], arr[min]);
+        swap(arr[i], arr[mini]);
+    }
+}
+
+int main()
+{
+    int size;
+    cout << "Enter the size of array : ";
+    cin >> size;
+
+    int arr[size];
+    cout << "Enter array : ";
+    for (int i = 0; i < size; i++)
+    {
+        cin >> arr[i];
     }
 
+    selectionSort(arr, size);
+
+    cout << "Sorted Array \n";
     for (int i = 0; i < size; i++)
     {
         cout << arr[i] << " ";
