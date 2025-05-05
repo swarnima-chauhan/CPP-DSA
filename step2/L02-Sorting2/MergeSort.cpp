cf53b0f3faf3f130e929fd92dbc25bb3d298fefe
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-06 05:05:55 +0530

    Merge sort

--- step2/L02-Sorting2/MergeSort.cpp
+++ step2/L02-Sorting2/MergeSort.cpp
@@ -1,8 +1,81 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void merg(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mS(vector<int> &arr, int low, int high)
+{
+    if (low == high)
+    {
+        return;
+    }
+    int mid = (low + high) / 2;
+    mS(arr, low, mid);
+    mS(arr, mid + 1, high);
+    merg(arr, low, mid, high);
+}
+
+void mergeSort(vector<int> &arr, int n)
+{
+    int low = 0;
+    int high = n - 1;
+    mS(arr, low, high);
+}
+
 int main()
 {
+    int n;
+    cout << "Enter size of array : ";
+    cin >> n;
+    cout << "Enter array : ";
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
