45caa4cd7ea6d586f07b06651904f135545f61bc
swarnima-chauhan <swarnimc130101@gmail.com>
2025-05-09 02:35:42 +0530

    table of 2

---
+++ step1/prime.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << '2' << '*' << i << '=' << i * 2 << endl;
+    }
+    return 0;
+}
